eb5aa05d2b0b1ef29bef75a50930fd031b744b69
Abbasi Solutions <contact@abbasaisolutions.com>
2026-07-05 15:33:34 -0500

    Allow Google sign-in popup messaging

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -24,6 +24,19 @@ const nextConfig = {
             },
         }
         : {}),
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin-allow-popups',
+                    },
+                ],
+            },
+        ];
+    },
     ...(outputMode
         ? {}
         : {
